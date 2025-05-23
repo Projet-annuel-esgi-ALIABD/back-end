1e38ecd0d29637a71b7acc63f67850a283997cfa
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2025-05-23 17:15:24 +0200

    Fix Cors to true

--- settings.py
+++ settings.py
@@ -7,10 +7,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False  # Change this to False for specific origins
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',  # Add your frontend's development URL
-    'https://dev-meteo-du-sinj.apps.fgib.fr',  # Add your frontend's production URL
-]
+CORS_ALLOW_ALL_ORIGINS = True  # Change this to False for specific origins
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_EXPOSE_HEADERS = ['Content-Type', 'Authorization']
